d997d062625f3aed34c1cb83e928b0152d83e763
Tacapteh <theo.chiron@ynov.com>
2025-08-01 10:51:58 +0200

    dd

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,14 @@
 @echo off
 cd /d "%~dp0"
+
+echo =============== Étape 1 : build ===============
 cd frontend
-npm run build
+call npm run build
 cd ..
+
+echo =============== Étape 2 : deploy ==============
 firebase deploy
-pause
+
+echo.
+echo ✅ Terminé. Appuie sur une touche pour quitter...
+pause >nul
